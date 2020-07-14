fa0f1737bb05f27a39a6afab36e57095cc006915
capeta0507 <nintendof1@gmail.com>
2020-07-14 21:03:45 +0800

    mongodb password

--- mongodb_altas.js
+++ mongodb_altas.js
@@ -1,5 +1,5 @@
 // Mongodb Altas Movies 連線 要有 { useNewUrlParser: true } 選項 
-var mongodb_url = 'mongodb+srv://davidtpe:b2UP0XdnV52UD8ey@mcu-movies-vgmaf.gcp.mongodb.net/Marvel_db?retryWrites=true&w=majority';
+var mongodb_url = 'mongodb+srv://davidtpe:<password>@mcu-movies-vgmaf.gcp.mongodb.net/Marvel_db?retryWrites=true&w=majority';
 var databaseName = "Marvel_db";  // DB
 var collecionName = 'Movies';    // Collection
 module.exports = {
